a6b1eaf12452ca7ae167aa8da360374b10c5f6e5
Sayantan Golder <sayantangolder1710@gmail.com>
2026-06-22 17:35:13 +0530

    Google callback URL changed

--- config/passport.js
+++ config/passport.js
@@ -31,7 +31,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:8080/auth/google/callback"
+            callbackURL: process.env.GOOGLE_CALLBACK_URL
         },
         async(accessToken, efreshToken, profile, done) => {
             try{
